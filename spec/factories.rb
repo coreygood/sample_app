38061bc0b368d89efb1ae97f5963a6a580e4f4f5
Corey Good <corey.good@sendgrid.com>
2013-07-09 14:09:17 -0600

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,16 @@
 FactoryGirl.define do
 	factory :user do
-		name "Corey Good"
-		email	"coreyg@good.com"
+		sequence(:name) { |n| "Person #{n}" }
+		sequence(:email) { |n| "person_#{n}@example.com"}
 		password "foobar"
 		password_confirmation "foobar"
+
+		factory :admin do
+			admin true
+		end
+		# name "Corey Good"
+		# email	"coreyg@good.com"
+		# password "foobar"
+		# password_confirmation "foobar"
 	end
 end
